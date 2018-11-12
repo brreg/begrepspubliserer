9547888bf0adabd44134234d3366d333ea30e5f6
Frode Roxrud Gill <extfrg@brreg.no>
2018-11-12 13:33:13 +0100

    Remove unused code

--- src/test/java/no/brreg/begrep/exceptions/ExtractExceptionTest.java
+++ src/test/java/no/brreg/begrep/exceptions/ExtractExceptionTest.java
@@ -1,32 +1,9 @@
 package no.brreg.begrep.exceptions;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.sun.javaws.exceptions.InvalidArgumentException;
-import no.brreg.begrep.Application;
-import no.brreg.begrep.controller.BegrepController;
-import no.brreg.begrep.exceptions.ExtractException;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.riot.RIOT;
-import org.apache.jena.vocabulary.RDF;
-import org.apache.jena.vocabulary.SKOS;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.charset.StandardCharsets;
-
-import static org.mockito.Mockito.*;
 
 
 @RunWith(SpringRunner.class)
