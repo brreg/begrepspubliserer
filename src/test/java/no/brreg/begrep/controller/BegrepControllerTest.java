be41896e37f0e150059f06ba07811946ac3d8059
Frode Roxrud Gill <extfrg@brreg.no>
2020-01-21 14:48:00 +0100

    More unitTest fixes

--- src/test/java/no/brreg/begrep/controller/BegrepControllerTest.java
+++ src/test/java/no/brreg/begrep/controller/BegrepControllerTest.java
@@ -11,21 +11,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 public class BegrepControllerTest {
 
-    private BegrepController begrepController = new BegrepController();
-
     @Test
     public void emptyAcceptHeaderShouldReturnNull() {
+        BegrepController begrepController = new BegrepController();
         Assert.assertEquals(null, begrepController.negotiateMimeType(""));
         Assert.assertEquals(null, begrepController.negotiateMimeType(null));
     }
 
     @Test
     public void nonOverlappingAcceptHeaderShouldReturnNull() {
+        BegrepController begrepController = new BegrepController();
         Assert.assertEquals(null, begrepController.negotiateMimeType("text/plain"));
     }
 
     @Test
     public void jsonHasPriority() {
+        BegrepController begrepController = new BegrepController();
         Assert.assertEquals(BegrepController.JSON_MIMETYPE, begrepController.negotiateMimeType("application/json"));
         Assert.assertEquals(BegrepController.JSON_MIMETYPE, begrepController.negotiateMimeType("*/*"));
         Assert.assertEquals(BegrepController.JSON_MIMETYPE, begrepController.negotiateMimeType("text/plain,*/*"));
@@ -36,22 +37,26 @@ public class BegrepControllerTest {
 
     @Test
     public void testWildcard() {
+        BegrepController begrepController = new BegrepController();
         Assert.assertEquals(BegrepController.JSON_MIMETYPE, begrepController.negotiateMimeType("text/turtle;q=0.9,application/json;q=0.8,application/*;q=1.0"));
     }
 
     @Test
     public void testDefaultQuality() {
+        BegrepController begrepController = new BegrepController();
         Assert.assertEquals(BegrepController.TURTLE_MIMETYPE, begrepController.negotiateMimeType("application/json;q=0.9,application/rdf+xml;q=0.9,text/turtle"));
     }
 
     @Test
     public void testQuality() {
+        BegrepController begrepController = new BegrepController();
         Assert.assertEquals(BegrepController.TURTLE_MIMETYPE, begrepController.negotiateMimeType("application/json;q=0.8,text/turtle;asdf=asdf;q=0.9"));
         Assert.assertEquals(BegrepController.JSON_MIMETYPE, begrepController.negotiateMimeType("application/json;q=asdf"));
     }
 
     @Test
     public void testPing() {
+        BegrepController begrepController = new BegrepController();
         ResponseEntity<String> response = begrepController.getPing();
         Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
         Assert.assertEquals("pong", response.getBody());
@@ -59,6 +64,7 @@ public class BegrepControllerTest {
 
     @Test
     public void testReadyWhenNotReady() {
+        BegrepController begrepController = new BegrepController();
         ResponseEntity response = begrepController.getReady();
         Assert.assertEquals(HttpStatus.SERVICE_UNAVAILABLE.value(), response.getStatusCodeValue());
 
@@ -66,6 +72,7 @@ public class BegrepControllerTest {
 
     @Test
     public void testGetBegreperNoMimeType() {
+        BegrepController begrepController = new BegrepController();
         ResponseEntity<String> response = begrepController.getBegreper("");
         Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
         final String expected = "Please use Accept:-header with mime-type";
@@ -74,6 +81,7 @@ public class BegrepControllerTest {
 
     @Test
     public void testGetBegreperNullMimeType() {
+        BegrepController begrepController = new BegrepController();
         ResponseEntity<String> response = begrepController.getBegreper(null);
         Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
         final String expected = "Please use Accept:-header with mime-type";
@@ -82,6 +90,7 @@ public class BegrepControllerTest {
 
     @Test
     public void testGetBegreperTextTurtleNotReady() {
+        BegrepController begrepController = new BegrepController();
         ResponseEntity<String> response = begrepController.getBegreper("text/turtle");
         Assert.assertEquals(HttpStatus.SERVICE_UNAVAILABLE.value(), response.getStatusCodeValue());
     }
